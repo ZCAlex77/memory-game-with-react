2c3f0ff9249e16dc5d933505514882bf8feacc7b
Alex <alexandruzmau320@gmail.com>
2023-05-20 19:19:55 +0300

    Update help section

--- src/components/Help.js
+++ src/components/Help.js
@@ -8,14 +8,18 @@ export const Help = ({ help, toggleHelp }) => {
       <button onClick={toggleHelp}>X</button>
       <hr></hr>
       <ul>
-        <li>You must choose a card that hasn't been choosen yet.</li>
         <li>
-          Choosing a card that has already been choosen will make you lose.
+          The game consists of multiple game cycles, each one lasting until all
+          the cards have been clicked on once.
         </li>
-        <li>Choosing a card for the first time will bring you points.</li>
+        <li>Clicking on a card for the first time will bring you points.</li>
         <li>
-          Every 12 points scored, all the visited cards will reset, allowing you
-          to start again.
+          If you click a card for the second time in the same game cycle, the
+          game will restart.
+        </li>
+        <li>
+          Once a game cycle ends, all the cards that have been clicked reset,
+          allowing you to click on them again.
         </li>
       </ul>
     </div>
